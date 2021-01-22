268767de77cba807ae6d9c9337bedd8d671c60e7
andy-aguilar <aaguil3@gmail.com>
2021-01-22 13:43:23 -0600

    commit tree 225b2f85c5c32eb764a7fe6fdb660cbc5c64c88c

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-    return word.scan(/^[aeiouAEIOU]/).length > 0
+    return !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
